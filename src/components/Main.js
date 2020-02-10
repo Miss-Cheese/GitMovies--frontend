01b211c9085623fc73132fa183541a5943f3fcef
Miss-Cheese <nat.galligan@gmail.com>
2020-02-10 10:17:59 -0500

    Setup main components

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Login from './Login';
+
+class Main extends React.Component {
+
+    render () {
+        return (
+            <div>
+            <h1>Welcome to Git Movies!</h1>
+            <Login />
+        </div>
+    )
+    }
+}
+
+export default Main
